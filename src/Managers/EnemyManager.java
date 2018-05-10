a720f8ac9a217a6709caea5b2f2044ce5a237087
NiteMannn <38901224+NiteMannn@users.noreply.github.com>
2018-05-09 18:14:47 -0700

    Added javadoc comments

--- src/Managers/EnemyManager.java
+++ src/Managers/EnemyManager.java
@@ -16,22 +16,42 @@ public class EnemyManager {
 	private int max;// maximum amount of enemies
 	private int enemyIndex = 0;
 
+	/**
+	 * Loaded constructor - initializes enemies array list
+	 * @param enemies - array list of enemies
+	 */
 	public EnemyManager(ArrayList<Enemy> enemies) {
 		this.enemies = enemies;
 	}
 
+	/**
+	 * Gets enemy array list
+	 * @return enemies - enemy array list
+	 */
 	public ArrayList<Enemy> getList() {
 		return enemies;
 	}
 
+	/**
+	 * Gets index of enemy
+	 * @return enemyIndex - index of enemy
+	 */
 	public int getEnemyIndex() {
 		return enemyIndex;
 	}
 
+	/**
+	 * Gets index of enemy
+	 * @param enemyIndex - index of enemy
+	 */
 	public void setEnemyIndex(int enemyIndex) {
 		this.enemyIndex = enemyIndex;
 	}
 
+	/**
+	 * Renders enemies
+	 * @param g - graphics drawer
+	 */
 	public void draw(Graphics g) {
 		for (Enemy e : enemies)
 			e.draw(g);
