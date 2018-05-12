419c1827294308df851b22980e04bd26e2117451
solemnote <36023183+solemnote@users.noreply.github.com>
2018-05-12 09:28:46 -0700

    Add files via upload

--- src/CaveLevels/Level231.java
+++ src/CaveLevels/Level231.java
@@ -1,9 +1,23 @@
 package CaveLevels;
 
+import java.util.Random;
+
 import Levels.LevelCreator;
 
 public class Level231 extends LevelCreator {
+	private Random r = new Random();
+
 	public Level231() {
-		
+		setImage("Images\\cave.png");
+
+		for (int i = 0; i < 10; i++) {
+			setImage(r.nextInt(8), r.nextInt(19), "Images\\rockOnCave.png", true);
+			setImage(r.nextInt(7) + 13, r.nextInt(19), "Images\\rockOnCave.png", true);
+		}
+
+		for (int k = 0; k < 21; k++) {
+			setImage(k, 0, "Images\\caveWall.png", true);
+			setImage(0, k, "Images\\caveWall.png", true);
+		}
 	}
 }
--- src/CaveLevels/Level241.java
+++ src/CaveLevels/Level241.java
@@ -1,9 +1,33 @@
 package CaveLevels;
 
+import java.util.Random;
+
 import Levels.LevelCreator;
 
 public class Level241 extends LevelCreator {
+	private Random r = new Random();
+	
 	public Level241() {
+		setImage("Images\\cave.png");
 		
+		for(int i = 0; i < 10; i++) {
+			setImage(r.nextInt(8), r.nextInt(19), "Images\\rockOnCave.png", true);
+			setImage(r.nextInt(7)+13, r.nextInt(19), "Images\\rockOnCave.png", true);
+		}
+		
+		for(int k = 0; k < 21; k++) {
+			setImage(k,0, "Images\\caveWall.png",true);
+			setImage(20,k, "Images\\caveWall.png",true);
+		}
+		setImage(17,1,"Images\\cave.png",false);
+		setImage(17,2,"Images\\cave.png",false);
+		setImage(17,3,"Images\\cave.png",false);
+		setImage(18,1,"Images\\cave.png",false);
+		setImage(18,2, "Images\\door.png",false);//boss level door
+		setImage(18,3,"Images\\cave.png",false);
+		setImage(19,1,"Images\\cave.png",false);
+		setImage(19,2,"Images\\cave.png",false);
+		setImage(19,3,"Images\\cave.png",false);
+
 	}
 }
