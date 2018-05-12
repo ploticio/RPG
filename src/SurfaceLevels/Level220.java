a6c7cb869691d1ba411e994e308c449ccd70d0cd
solemnote <36023183+solemnote@users.noreply.github.com>
2018-05-12 09:35:13 -0700

    houses

--- src/SurfaceLevels/Level220.java
+++ src/SurfaceLevels/Level220.java
@@ -39,5 +39,55 @@ public class Level220 extends LevelCreator {
 		for (int k = 13; k < 21; k++) {
 			setImage(k, 20, "Images\\bushHo.png", true);
 		}
+		
+		setImage(3,14,"Images\\house1.png",true);
+		setImage(4,14,"Images\\house2.png",true);
+		setImage(5,14,"Images\\house3.png",true);
+		setImage(3,15,"Images\\house4.png",true);
+		setImage(4,15,"Images\\house5.png",true);
+		setImage(5,15,"Images\\house6.png",true);
+		setImage(3,16,"Images\\house7.png",true);
+		setImage(4,16,"Images\\house8.png",true);
+		setImage(5,16,"Images\\house9.png",true);
+		
+		setImage(3,2,"Images\\house1.png",true);
+		setImage(4,2,"Images\\house2.png",true);
+		setImage(5,2,"Images\\house3.png",true);
+		setImage(3,3,"Images\\house4.png",true);
+		setImage(4,3,"Images\\house5.png",true);
+		setImage(5,3,"Images\\house6.png",true);
+		setImage(3,4,"Images\\house7.png",true);
+		setImage(4,4,"Images\\house8.png",true);
+		setImage(5,4,"Images\\house9.png",true);
+		
+		setImage(13,2,"Images\\house1.png",true);
+		setImage(14,2,"Images\\house2.png",true);
+		setImage(15,2,"Images\\house3.png",true);
+		setImage(13,3,"Images\\house4.png",true);
+		setImage(14,3,"Images\\house5.png",true);
+		setImage(15,3,"Images\\house6.png",true);
+		setImage(13,4,"Images\\house7.png",true);
+		setImage(14,4,"Images\\house8.png",true);
+		setImage(15,4,"Images\\house9.png",true);
+		
+		setImage(13,8,"Images\\house1.png",true);
+		setImage(14,8,"Images\\house2.png",true);
+		setImage(15,8,"Images\\house3.png",true);
+		setImage(13,9,"Images\\house4.png",true);
+		setImage(14,9,"Images\\house5.png",true);
+		setImage(15,9,"Images\\house6.png",true);
+		setImage(13,10,"Images\\house7.png",true);
+		setImage(14,10,"Images\\house8.png",true);
+		setImage(15,10,"Images\\house9.png",true);
+		
+		setImage(3,8,"Images\\house1.png",true);
+		setImage(4,8,"Images\\house2.png",true);
+		setImage(5,8,"Images\\house3.png",true);
+		setImage(3,9,"Images\\house4.png",true);
+		setImage(4,9,"Images\\house5.png",true);
+		setImage(5,9,"Images\\house6.png",true);
+		setImage(3,10,"Images\\house7.png",true);
+		setImage(4,10,"Images\\house8.png",true);
+		setImage(5,10,"Images\\house9.png",true);
 	}
 }
